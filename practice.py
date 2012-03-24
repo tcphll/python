1e3a0946d224483c92d36c104ec2959d5148f9fc
Casey Phillips <public@peench.com>
2012-03-23 21:24:58 -0500

    Initial commit

---
+++ practice.py
@@ -0,0 +1,10 @@
+crap = 101
+
+print "Python is weird, but I kinda like it."
+if crap > 10:
+	print "Ok, this is simple"
+else:
+	print "Ok, this is not so simple"
+	
+
+	
---
+++ test.py
@@ -0,0 +1,26 @@
+names = ['This', 'is', 'an', 'array']
+for name in range(len(names)):
+  print names[name]
+
+if 5 > 0:
+	print"This is bad"
+	print"nevermind, this is AWESOME!"
+
+if 10 < 20:
+	print"Fuck off"
+	
+fart = 15
+
+if fart > 5:
+	print "fart is big"
+	print "And this is still in the thing"
+else:
+	print "Fart is small"
+	
+	
+for x in range(0, fart):
+	print x
+else:
+	print "We're done!"
+	print "Python is cool!"
+	
